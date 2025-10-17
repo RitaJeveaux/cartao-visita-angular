c2d3fab4022fa7c4bb3efb2843172abbf4c453ee
RitaJeveaux <ritajeveaux@gmail.com>
2025-10-17 11:22:18 -0300

    finalizando projeto

--- src/app/components/card/profile-info/profile-info.ts
+++ src/app/components/card/profile-info/profile-info.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile-info',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile-info.html',
   styleUrl: './profile-info.css'
 })
 export class ProfileInfo {
-
+  nome: string = 'Rita Jeveaux';
+  titulo: string = 'Desenvolvedora front-end';
+  descricao: string = 'Estou a disposição para novos projetos.'
+  skills: string[] = ['HTML', 'CSS', 'Typescript', 'Angular', 'React'];
 }
